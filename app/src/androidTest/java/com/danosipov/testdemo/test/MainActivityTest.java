0782de3a3dbe891ce3d452a7c42d208d021b80d2
Dan Osipov <dan@osipov.org>
2015-01-26 18:18:30 -0800

    Few more unit tests

--- app/src/androidTest/java/com/danosipov/testdemo/test/MainActivityTest.java
+++ app/src/androidTest/java/com/danosipov/testdemo/test/MainActivityTest.java
@@ -1,29 +1,91 @@
 package com.danosipov.testdemo.test;
 
 import android.app.Activity;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.danosipov.testdemo.MainActivity;
 import com.danosipov.testdemo.R;
+import com.danosipov.testdemo.TemperatureEditText;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 @Config(emulateSdk = 18, manifest = "app/src/main/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
+    Activity activity;
+    TemperatureEditText fahrenheitField;
+    TemperatureEditText celsiusField;
+
+    @Before
+    public void setUp() {
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        fahrenheitField = (TemperatureEditText) activity.findViewById(R.id.fahrenheit);
+        celsiusField = (TemperatureEditText) activity.findViewById(R.id.celsius);
+    }
+
     @Test
     public void launchedActivity_shouldNotBeNull() throws Exception {
-        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
         assertThat(activity, notNullValue());
+    }
+
+    @Test
+    public void textInFahrenheit_shouldBeConvertedToCelsius() throws Exception {
+        fahrenheitField.requestFocusFromTouch();
+        fahrenheitField.setText("32");
+
+        assertEquals("0.0", celsiusField.getText().toString());
+    }
+
+    @Test
+    public void textInCelsius_shouldBeConvertedToFahrenheit() throws Exception {
+        celsiusField.requestFocusFromTouch();
+        celsiusField.setText("0");
+
+        assertEquals("32.0", fahrenheitField.getText().toString());
+    }
+
+    @Test
+    public void negativeTemperatureInCelsius_shouldBeConvertedToFahrenheit() throws Exception {
+        celsiusField.requestFocusFromTouch();
+        celsiusField.setText("-50");
+
+        assertEquals("-58.0", fahrenheitField.getText().toString());
+    }
+
+    @Test
+    public void negativeTemperatureInFahrenheit_shouldBeConvertedToCelsius() throws Exception {
+        fahrenheitField.requestFocusFromTouch();
+        fahrenheitField.setText("-32");
+
+        assertEquals("-35.6", celsiusField.getText().toString());
+    }
+
+    @Test
+    public void backAndForthConversions_shouldYieldCorrectValues() throws Exception {
+        fahrenheitField.requestFocusFromTouch();
+        fahrenheitField.setText("0");
+        celsiusField.requestFocusFromTouch();
+        celsiusField.setText("100");
+
+        assertEquals("212.0", fahrenheitField.getText().toString());
+        assertEquals("100", celsiusField.getText().toString());
+    }
+
+    @Test
+    public void entryWithoutFocus_shouldResultInNoChange() throws Exception {
+        fahrenheitField.requestFocusFromTouch();
+        fahrenheitField.setText("50");
+        celsiusField.setText("100");
+
+        assertEquals("50", fahrenheitField.getText().toString());
     }
 }
